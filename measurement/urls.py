85eb0b268d66433e2af77152b8a049077bba2271
Psycho <ilya.efremov.0498@gmail.com>
2024-02-01 16:40:43 +0500

    IV commit

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import SensorView, MeasurementView, SensorsRetrieve
+from .views import SensorView, MeasurementView, SensorChange
 
 urlpatterns = [
     path('sensors/', SensorView.as_view()),
     path('measurements/', MeasurementView.as_view()),
-    path('sensors/<pk>', SensorsRetrieve.as_view()),
+    path('sensors/<pk>', SensorChange.as_view()),
 ]
